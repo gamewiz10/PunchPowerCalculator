28e473150bac56a77b0fd8a24774ae108ac1f2d5
gamewiz10 <joseph.bell.20@cnu.edu>
2023-10-03 00:37:32 -0400

    innit commit

---
+++ app/src/main/java/com/example/punchpowercalculator/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.punchpowercalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openCalculator(View view){
+        Intent intent = new Intent(this, PreCalculator.class);
+        startActivity(intent);
+    }
+
+    public void openLogbook(View view) {
+        Intent intent = new Intent(this, Logbook.class);
+        startActivity(intent);
+    }
+}
